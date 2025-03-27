7b5f341a7bda560d2f843a0609b56ccf73866d8d
oliguilherme <guilhermeleitesouza2018@gmail.com>
2025-03-26 21:52:57 -0300

    sla

--- main.c
+++ main.c
@@ -13,7 +13,11 @@ typedef struct {
 void imprimirInterface();
 void processarOpcao(Aluno alunos[], int opcao, int *qnt);
 
+
 void cadastrarAluno(Aluno alunos[], int *qnt);
+void exibirAluno(Aluno alunos[], int *qnt);
+void editarAluno(Aluno alunos[], int qnt, int id);
+
 
 int main() {
     system("cls");
@@ -30,8 +34,8 @@ int main() {
         getchar();
         processarOpcao(alunos, opcao, &qnt);
 
-         printf("\nPressione ENTER para continuar...");
-         getchar();
+        printf("\nPressione ENTER para continuar...");
+        getchar(); getchar();
     } while (opcao != 5);
 
     return 0;
@@ -47,15 +51,18 @@ void imprimirInterface() {
 }
 
 void processarOpcao(Aluno alunos[], int opcao, int *qnt) {
+    int id;
     switch (opcao) {
         case 1:
             cadastrarAluno(alunos, qnt);
             break;
         case 2:
-            
+            exibirAluno(alunos, qnt);
             break;
         case 3:
-            
+            printf("Informe o ID: ");
+            scanf("%d", &id);
+            editarAluno(alunos, qnt, id);
             break;
         case 4:
             
@@ -80,9 +87,8 @@ void cadastrarAluno(Aluno alunos[], int *qnt) {
 
     printf("Nome do aluno: ");
     fgets(novoAluno.nome, 101, stdin);
-    //novoAluno.nome[strcspn(novoAluno.nome, "\n")] = '\0';
+    novoAluno.nome[strcspn(novoAluno.nome, "\n")] = '\0';
     
-    getchar();
     printf("ID de %s: ", novoAluno.nome);
     scanf("%d", &novoAluno.id);
 
@@ -103,4 +109,67 @@ void cadastrarAluno(Aluno alunos[], int *qnt) {
     (*qnt)++;
 
     printf("\nAluno cadastrado com sucesso!\n");
+}
+
+void exibirAluno(Aluno alunos[],int *qnt){
+    int i = 0;  
+
+    if (*qnt == 0) {
+        printf("Nenhum aluno cadastrado no sistema.\n");
+    } else {
+        do {
+            printf("\nNome: %s", alunos[i].nome);
+            printf("\nID: %d", alunos[i].id);
+            printf("\nNota: %.2f", alunos[i].nota);
+            printf("\nMatricula: %s", alunos[i].matricula);
+            i++;        
+        } while (i < *qnt); 
+    }
+
+    printf("Quantidade de alunos: %d", *qnt);//mostra quantidade de alunos
+    
+}
+
+void editarAluno(Aluno alunos[], int qnt, int id) {
+    int opcao;
+
+    if (qnt == 0) {
+        printf("Nenhum aluno cadastrado no sistema.\n");
+    } else {
+        for (int i = 0; i < qnt; i++) {
+            if (id == alunos[i].id) {
+                printf("Qual campo deseja alterar?\n");
+                printf("1 - Nome\n2 - Matricula\n3 - Nota\n");
+                scanf("%d", &opcao);
+    
+                switch (opcao) {
+                    case 1:
+                        printf("Nome atual: %s", alunos[i].nome);
+                        printf("\nNovo nome: ");
+                        fgets(alunos[i].nome, 101, stdin);
+                        alunos[i].nome[strcspn(alunos[i].nome, "\n")] = '\0';
+                        break;
+                    case 2:
+                        printf("Matricula atual: %s", alunos[i].matricula);
+                        printf("\nNova matricula: ");
+                        fgets(alunos[i].matricula, 6, stdin);
+                        getchar();
+                        break;
+                    
+                    case 3:
+                    getchar();
+                        printf("Nota atual: %f", alunos[i].nota);
+                        
+                        do {
+                            printf("\nNova nota: ");
+                            scanf("%f", &alunos[i].nota);
+                        } while(alunos[i].nota > 10.0 || alunos[i].nota < 0.0);
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+        } 
+    }
 }
